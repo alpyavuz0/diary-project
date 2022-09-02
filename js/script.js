71d64ff2da9fb6e2b56bdb393fcfd571d7a9b478
Alper Yavuz <thecomp00@gmail.com>
2022-09-02 20:25:07 +0300

    'nav-color-js-changed'

--- js/script.js
+++ js/script.js
@@ -16,14 +16,15 @@ document.addEventListener("click", e => {
 
 function changeColor () {
     let navbar = document.querySelector('[changeColor]')
+    let drop = document.querySelector('[dropChange]')
     let button = document.getElementsByClassName(".nv4btn")
     let scrollValue = window.scrollY;
     if(scrollValue < 395){
         navbar.classList.remove('changeColor');
-       
+        drop.classList.remove('changeColor');
     } else { 
         navbar.classList.add('changeColor');
- 
+        drop.classList.add('changeColor');
     }
 
     if(scrollValue < 395) {
@@ -40,13 +41,17 @@ window.addEventListener('scroll', changeColor)
 
 function changeNav () {
     let navz = document.querySelector('.nav')
+    let dropz = document.querySelector('[dropChange]')
     let btn = document.getElementsByClassName(".nv4btn")
     let scrollValue = window.scrollY;
     if(scrollValue < 1) {
         navz.classList.remove('changeNav')
+        dropz.classList.remove('changeNav')
     } else {
         navz.classList.add('changeNav')
+        dropz.classList.add('changeNav')
     }
 }
 
 window.addEventListener('scroll', changeNav)
+
